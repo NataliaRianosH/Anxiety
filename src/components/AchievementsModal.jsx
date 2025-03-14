457ea84a1b52d961a73ee62005e60632e3a445c2
Natalia <rianos.natalia@correounivalle.edu.co>
2025-03-13 21:11:15 -0500

    detalles del logro

--- src/components/AchievementsModal.jsx
+++ src/components/AchievementsModal.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaTrophy } from "react-icons/fa";
 import "../assets/styles/AchievementsModal.scss";
 import AchievementCard from "./AchievementCard";
+import AchievementDetails from "./AchievementDetails";
 import { useAchievements } from "../context/AchievementsContext"; 
+
 const AchievementsModal = ({ isOpen, onClose }) => {
   const { achievements } = useAchievements();
   if (!isOpen) return null;
+
+  const [selectedAchievement, setSelectedAchievement] = useState(null);
 
   return (
     <div className="achievements-modal">
@@ -15,11 +19,24 @@ const AchievementsModal = ({ isOpen, onClose }) => {
           <button className="close-button" onClick={onClose}>✖</button>
         </div>
 
-        <div className="achievements-grid">
-          {achievements.map((achievement) => (
-            <AchievementCard key={achievement.id} achievement={achievement} />
-          ))}
-        </div>
+        {/* Si hay un logro seleccionado, mostrar los detalles */}
+        {selectedAchievement ? (
+          <AchievementDetails 
+            achievement={selectedAchievement} 
+            onBack={() => setSelectedAchievement(null)} 
+          />
+        ) : (
+          // Si no hay un logro seleccionado, mostrar las tarjetas
+          <div className="achievements-grid">
+            {achievements.map((achievement) => (
+              <AchievementCard  
+                key={achievement.id} 
+                achievement={achievement} 
+                onClick={() => setSelectedAchievement(achievement)} 
+              />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
