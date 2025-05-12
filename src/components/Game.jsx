cd0a4f7a4708c7a4130d9dd7ca9c6000ce816e5e
Natalia <rianos.natalia@correounivalle.edu.co>
2025-05-12 01:11:20 -0500

    prueba

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -22,6 +22,7 @@ import LearningModal from "../components/modals/LearningModal"; //  importar el 
 import learningData from "../components/modals/learningData";
 import GameCompleteModal from "./modals/GameCompleteModal";
 import ResizeHandler from "./ResizeHandler";
+import { Perf } from "r3f-perf";
 
 
 const Game = () => {
@@ -87,6 +88,7 @@ const Game = () => {
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000, position: [-50, 10, 21] }}
       >
+        <Perf position="top-left" />
         <ResizeHandler />
         {/** camera={{ position: [0, 0, 10], near: 0.1, far: 1000 }}  **/}
         {/** <OrbitControls enableZoom={true} enablePan={true} />**/}
